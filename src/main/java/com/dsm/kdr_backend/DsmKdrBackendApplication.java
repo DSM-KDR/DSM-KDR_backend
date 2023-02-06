41c030d075b6caa4ed787363a036457dd20e1c69
hee <cmlgml094356@gmail.com>
2023-02-06 18:40:17 +0900

    setting : 프로젝트 세팅

---
+++ src/main/java/com/dsm/kdr_backend/DsmKdrBackendApplication.java
@@ -0,0 +1,13 @@
+package com.dsm.kdr_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DsmKdrBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DsmKdrBackendApplication.class, args);
+	}
+
+}
