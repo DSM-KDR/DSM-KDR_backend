4cc28d38a8c3e26c317a029dc9e292008e5c7b64
hee <cmlgml094356@gmail.com>
2023-02-20 23:48:27 +0900

    update : EnableJpaAuditing 어노테이션 추가
    - BaseTimeEntity에서 EntityListeners(AuditingEntityListener.class) 어노테이션을 사용하기 때문에 필요
    - BaseIdEntity에서는 EntityListeners 어노테이션이 필요하지 않음.

--- src/main/java/com/dsm/kdr_backend/DsmKdrBackendApplication.java
+++ src/main/java/com/dsm/kdr_backend/DsmKdrBackendApplication.java
@@ -4,8 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.context.ApplicationContext;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 import org.springframework.web.servlet.DispatcherServlet;
 
+@EnableJpaAuditing
 @SpringBootApplication
 public class DsmKdrBackendApplication extends SpringBootServletInitializer {
 
--- src/main/java/com/dsm/kdr_backend/global/entity/BaseIdEntity.java
+++ src/main/java/com/dsm/kdr_backend/global/entity/BaseIdEntity.java
@@ -1,18 +1,14 @@
 package com.dsm.kdr_backend.global.entity;
 
-import javax.persistence.EntityListeners;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.MappedSuperclass;
 
-import org.springframework.data.jpa.domain.support.AuditingEntityListener;
-
 import lombok.Getter;
 
 @Getter
 @MappedSuperclass
-@EntityListeners(AuditingEntityListener.class)
 public abstract class BaseIdEntity {
 
     @Id
